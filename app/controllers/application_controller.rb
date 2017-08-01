0bd68b7aa045fc25c7d78dc1cb03e60881deb04b
Chris Loftus <cwl@aber.ac.uk>
2017-08-01 16:08:29 +0100

    Version 1 with tabbing and pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
+# Top-level class for common controller code
+# @author Chris Loftus
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Make is_admin? a helper method available to view templates
+  helper_method :is_admin?
+
+  protected
+
+  # Is the caller an admin user? Currently everyone is!
+  # @return true if admin user else false
+  def is_admin?
+    true
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+1..40.times do |i|
+    User.create(surname:   "Surname#{i}",
+                firstname: "Firstname#{i}",
+                email:     "cwl#{i}@aber.ac.uk",
+                phone:     "01970 622422",
+                grad_year: 1985)
+end
